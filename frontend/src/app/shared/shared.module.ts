563944daab937a7ca5b62dc1fa1879fd51a63c10
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:20:23 +0300

    Created Header component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SomePipe } from './pipes/some.pipe';
 import { SomeDirective } from './directives/some.directive';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SomePipe, SomeDirective],
+  declarations: [SomePipe, SomeDirective, HeaderComponent],
   exports: [
     CommonModule,
     SomePipe,
