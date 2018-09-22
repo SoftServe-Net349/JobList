cb0023f13775388ee10f2e4640ccad778b201eae
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:15:09 +0300

    Created JobSearch component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import {ButtonModule} from 'primeng/button';
+import { JobSearchComponent } from './job-search/job-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobSearchComponent
   ],
   imports: [
     BrowserModule,
