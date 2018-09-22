4656ed0c7647f71f38cb1ebca05b40e02361a99f
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:07:32 +0300

    Created Recruiter component

---
+++ frontend/src/app/recruiter/recruiter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruiter',
+  templateUrl: './recruiter.component.html',
+  styleUrls: ['./recruiter.component.sass']
+})
+export class RecruiterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/recruiter/recruiter.module.ts
+++ frontend/src/app/recruiter/recruiter.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RecruiterRoutingModule } from './recruiter-routing.module';
+import { RecruiterComponent } from './recruiter.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RecruiterRoutingModule
   ],
-  declarations: []
+  declarations: [RecruiterComponent]
 })
 export class RecruiterModule { }
