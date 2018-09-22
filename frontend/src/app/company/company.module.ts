3895aebdfd4aac9eb7b4cca2be7b36aac6b4b423
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:03:24 +0300

    Created Company module wit routing

---
+++ frontend/src/app/company/company.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CompanyRoutingModule } from './company-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CompanyRoutingModule
+  ],
+  declarations: []
+})
+export class CompanyModule { }
