a1e661b70343ff6e7686fa7b810d63747889e4fd
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:05:36 +0300

    Created Recruiter module wit routing

---
+++ frontend/src/app/recruiter/recruiter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecruiterRoutingModule } from './recruiter-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RecruiterRoutingModule
+  ],
+  declarations: []
+})
+export class RecruiterModule { }
