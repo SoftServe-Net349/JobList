ba9ef545b65ba980f0fd10d8ccc7fbe6f4b18859
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:04:50 +0300

    Created Company component

--- frontend/src/app/company/company.module.ts
+++ frontend/src/app/company/company.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CompanyRoutingModule } from './company-routing.module';
+import { CompanyComponent } from './company.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CompanyRoutingModule
   ],
-  declarations: []
+  declarations: [CompanyComponent]
 })
 export class CompanyModule { }
