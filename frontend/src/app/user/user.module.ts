fa4202087f72b4c5d8df4cbc828d96f51370e167
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 09:59:54 +0300

    Created User component

--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UserRoutingModule
   ],
-  declarations: []
+  declarations: [UserComponent]
 })
 export class UserModule { }
