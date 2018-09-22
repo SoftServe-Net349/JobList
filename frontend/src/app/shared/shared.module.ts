c2abb8cc91509f68e0002a4ca08de53f66025f0d
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-09-22 10:21:10 +0300

    Created Footer component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SomePipe } from './pipes/some.pipe';
 import { SomeDirective } from './directives/some.directive';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SomePipe, SomeDirective, HeaderComponent],
+  declarations: [SomePipe, SomeDirective, HeaderComponent, FooterComponent],
   exports: [
     CommonModule,
     SomePipe,
