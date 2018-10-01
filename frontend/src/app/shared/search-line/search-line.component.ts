23ebfa1cf4a9e71c9eefc92b73685ef3c4f84a35
Pavlo Mykhailiak <mihailyk99@gmail.com>
2018-10-01 14:16:06 +0300

    updated searchline, dropdown with valid data

--- frontend/src/app/shared/search-line/search-line.component.ts
+++ frontend/src/app/shared/search-line/search-line.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+//import { City } from '../models/city.model'
+import { CityService } from '../../core/services/city.service';
 import {SelectItem} from 'primeng/api';
 
 
 @Component({
   selector: 'app-search-line',
   templateUrl: './search-line.component.html',
-  styleUrls: ['./search-line.component.sass']
+  styleUrls: ['./search-line.component.sass'],
+  providers: [CityService]
 })
 export class SearchLineComponent implements OnInit {
 
-  cities: SelectItem[];
-  selectedCar: string;
+  cities: City[];
+  selectedCity: City;
 
-  constructor() { //SelectItem API with label-value pairs
-    this.cities = [
-      {label: 'Lviv', value: 'Lviv'},
-      {label: 'Kyiv', value: 'Kyiv'},
-      {label: 'I-F', value: 'I-F'}
-      ];
+  constructor(private cityService:CityService) {
+
     }
 
   ngOnInit() {
+    this.loadCities();
   }
 
+  loadCities(){
+    this.cityService.getAll()
+    .subscribe((data: City[]) => this.cities = data);
+  }
+
+ 
+}
+class City {
+name: string;
 }
