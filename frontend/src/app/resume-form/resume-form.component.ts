238d30601704ec881f67c4c32eb5c06a43b203bb
Pavlo Paitak <pavlopaitak@gmail.com>
2018-10-04 19:52:50 +0300

    Created validation for user page and updated user form

--- frontend/src/app/resume-form/resume-form.component.ts
+++ frontend/src/app/resume-form/resume-form.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from '../shared/models/user.model';
+import { WorkAreaService } from '../core/services/work-area.service';
+import { CityService } from '../core/services/city.service';
+import { LanguageService } from '../core/services/language.service';
+import { SchoolService } from '../core/services/school.service';
+import { FacultyService } from '../core/services/faculty.service';
+import { WorkArea } from '../shared/models/work-area.model';
+import { Language } from '../shared/models/language.model';
+import { City } from '../shared/models/city.model';
+import { School } from '../shared/models/school.model';
+import { Faculty } from '../shared/models/faculty.model';
 
 @Component({
   selector: 'app-resume-form',
@@ -16,25 +26,67 @@ export class ResumeFormComponent implements OnInit {
 
   user: User;
 
-  showDialog() {
-    this.display = true;
-  }
+  workAreas: WorkArea[];
+  languages: Language[];
+  cities: City[];
+  schools: School[];
+  faculties: Faculty[];
 
-  constructor(private formBuilder: FormBuilder) {
+  selectedWorkArea: WorkArea;
+  selectedCity: City;
+  selectedLanguages: Language[];
+  birthDate: Date;
+  selectedShool1: School;
+  selectedFaculty1: Faculty;
+  edStartDate1: Date;
+  edFinishDate1: Date;
+  expStartDate: Date;
+  expFinishDate: Date;
+
+  constructor(private formBuilder: FormBuilder,
+              private workAreaService: WorkAreaService,
+              private cityService: CityService,
+              private languageService: LanguageService,
+              private schoolService: SchoolService,
+              private facultyService: FacultyService) {
 
     this.user = this.defaultUser();
 
-    this.resumeForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(150)]],
-      lastNmae: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      workArea: ['', [Validators.required]],
-      selectedCompanies: [''],
-      phone: ['']
-    });
   }
 
   ngOnInit() {
+    this.loadWorkAreas();
+    this.loadLanguages();
+    this.loadCities();
+    this.loadSchools();
+    this.loadFaculties();
+
+    this.resumeForm = this.defaultResumeForm();
+  }
+
+  loadWorkAreas() {
+    this.workAreaService.getAll()
+    .subscribe((data: WorkArea[]) => this.workAreas = data);
+  }
+
+  loadLanguages() {
+    this.languageService.getAll()
+    .subscribe((data: Language[]) => this.languages = data);
+  }
+
+  loadCities() {
+    this.cityService.getAll()
+    .subscribe((data: City[]) => this.cities = data);
+  }
+
+  loadSchools() {
+    this.schoolService.getAll()
+    .subscribe((data: School[]) => this.schools = data);
+  }
+
+  loadFaculties() {
+    this.facultyService.getAll()
+    .subscribe((data: Faculty[]) => this.faculties = data);
   }
 
   defaultUser(): User {
@@ -57,18 +109,58 @@ export class ResumeFormComponent implements OnInit {
     };
   }
 
+  defaultResumeForm(): FormGroup {
+    return this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(150)]],
+      phone: [''],
+      companyName: ['', [Validators.minLength(2), Validators.maxLength(200)]],
+      position: ['', [Validators.minLength(2), Validators.maxLength(200)]],
+      courses: [''],
+      familyState: ['', [Validators.minLength(2), Validators.maxLength(20)]],
+      softSkills: [''],
+      keySkills: [''],
+      linkedIn: ['', [Validators.minLength(2), Validators.maxLength(200)]],
+      gitHub: ['', [Validators.minLength(2), Validators.maxLength(200)]],
+      facebook: ['', [Validators.minLength(2), Validators.maxLength(200)]],
+      skype: ['', [Validators.minLength(2), Validators.maxLength(200)]]
+    });
+  }
+
   showResumeForm(action: String, user = this.defaultUser()) {
     this.user = user;
     this.resumeForm.reset();
     this.display = true;
     this.action = action;
-    // if (action === 'Update') {
-    //   this.resumeForm.setValue({
-    //     firstName: this.recruiter.firstName,
-    //     lastName: this.recruiter.lastName,
-    //     email: this.recruiter.email,
-    //     phone: this.recruiter.phone
-    //   });
-    // }
+
+    if (action === 'Update') {
+      this.resumeForm.setValue({
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        email: this.user.email,
+        phone: this.user.phone,
+        companyName: 'SoftServe',
+        position: '.NET DEVELOPER',
+        courses: this.user.resumes.courses,
+        familyState: this.user.resumes.familyState,
+        softSkills: this.user.resumes.softSkills,
+        keySkills: this.user.resumes.keySkills,
+        facebook: this.user.resumes.facebook,
+        skype: this.user.resumes.skype,
+        linkedIn: this.user.resumes.linkedin,
+        gitHub: this.user.resumes.github
+      });
+      this.selectedWorkArea = this.workAreas[1];
+      this.selectedCity = this.cities[1];
+      this.birthDate = new Date();
+      this.selectedShool1 = this.schools[1];
+      this.selectedFaculty1 = this.faculties[1];
+      this.edStartDate1 = new Date();
+      this.edFinishDate1 = new Date();
+      this.expStartDate = new Date();
+      this.expFinishDate = new Date();
+      this.selectedLanguages = this.languages;
+    }
   }
 }
