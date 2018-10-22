522ea416ad6a34d1149ec19ceba264e1eb44c0ae
tronbodya <tronbodya@gmail.com>
2018-10-22 12:36:21 +0300

    Solved mistakes

--- backend/JobList.BusinessLogic/Services/EmployeesService.cs
+++ backend/JobList.BusinessLogic/Services/EmployeesService.cs
@@ -137,7 +137,7 @@ namespace JobList.BusinessLogic.Services
             switch (field)
             {
                 case "birthDate":
-                    return e => e.BirthData.ToString();
+                    return e => e.BirthDate.ToString();
                 case "email":
                     return e => e.Email;
 
