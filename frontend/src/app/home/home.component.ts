9d7d6c88d4b5523448eeb8cd75e5e4f85389ab62
tronbodya <tronbodya@gmail.com>
2018-11-10 11:31:22 +0200

    Created UI for admin-chat panel

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.href = this.router.url;
 
-    if (this.href === '/admin' || this.href === '/chat') {
+    if (this.href === '/admin') {
       this.isShow = false;
     }
   }
